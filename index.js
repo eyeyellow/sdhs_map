802295d0d2c0b86b0742f505092236b29813b036
Frank Aiello <frankvaiello@gmail.com>
2022-09-24 16:08:13 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([42.585, -86.226], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
