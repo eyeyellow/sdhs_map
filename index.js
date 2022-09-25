db409ac377f0abfb03f898b65cbe71c18a533594
Frank Aiello <frankvaiello@gmail.com>
2022-09-25 11:51:27 -0400

    Adds folder info handling for CatalogIt API

--- index.js
+++ index.js
@@ -3,36 +3,32 @@
 // Class for holding the data
 class Location {
   constructor({ name, folder_id, coordinates }) {
-    this.title = name;
     this.folderId = folder_id;
     this.latLng = coordinates;
     this.marker = this.createMarker();
   }
   
   createMarker() {
-    return L.circleMarker(this.latLng, {
-      color: '#591845',
-      fillOpacity: 0.5,
-      radius: 5,
-      weight: 2
-    })
+    return L.marker(this.latLng)
+      // TODO - try to get the loading as a spinner
+      .bindPopup(L.popup({ folderId: this.folderId, content: 'Loading...' }));
   }
 }
 
 // Initialize raw data
 const locationsData = [
   {
-    name: 'Mt. Baldhead Radar Station',
+    // Mt. Baldhead Radar Station
     folder_id: '95533ed0-033a-11ec-9a57-1f0085eee116',
     coordinates: [42.661, -86.210]
   },
   {
-    name: 'Singapore, Michigan',
+    // Singapore, Michigan
     folder_id: 'c362ddb0-5df1-11eb-a191-f399729d14b9',
     coordinates: [42.677, -86.206]
   },
   {
-    name: 'Camp Gray',
+    // Camp Gray
     folder_id: '590f5ef0-6028-11eb-926c-21d02d648020',
     coordinates: [42.661, -86.215]
   }
@@ -62,3 +58,34 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Handle markers
 markers.forEach(marker => marker.addTo(map));
 map.fitBounds(bounds);
+
+// Bind event handlers to map
+map.on('popupopen', async (event) => {
+  const popup = event.popup;
+  // console.log(popup.options.folderId);
+  const folderInfo = await getFolderInfo(popup.options.folderId);
+  popup.setContent(getPopupInfo(folderInfo));
+});
+
+// Popup info template
+const getPopupInfo = function(folderInfo) {
+  let popupInfo = `<h3>${folderInfo.name}</h3>`;
+  if (folderInfo.description) {
+    popupInfo = popupInfo + `<div>${folderInfo.description}</div>`;
+  }
+  return popupInfo;
+}
+
+// CatalogIt API stuff
+const SDHS_ACCOUNT_ID = 4124;
+
+const folderRoute = function(folderId) {
+  return `https://api.catalogit.app/api/public/accounts/${SDHS_ACCOUNT_ID}/folders/${folderId}`;
+}
+
+const getFolderInfo = async function(folderId) {
+  const response = await fetch(folderRoute(folderId));
+  if (response.ok) {
+    return response.json();
+  }
+}
